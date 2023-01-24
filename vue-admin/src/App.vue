acdfd67ee3db1af4a4f2e15ce06854f199d7c9da
Yakov Lemenkov <yakoff4@gmail.com>
2023-01-24 23:08:45 +0300

    vue installed

---
+++ vue-admin/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <p>Name: {{ name }}</p>
+  <p>Job: {{ job }}</p>
+
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      name: 'Alex',
+      job: 'EPAM'
+    }
+  } 
+}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
